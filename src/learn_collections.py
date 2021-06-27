a63f606c9869292b4a8b1248d5fccec5bc5e2249
Deepa Joshi <deepabjoshi@gmail.com>
2021-06-27 08:29:04 +0530

    Done with collections.

--- src/learn_collections.py
+++ src/learn_collections.py
@@ -1,4 +1,5 @@
 from collections import *
+import re
 
 # ChainMap
 d1 = {'a': 1, 'c': 2}
@@ -84,3 +85,93 @@ print()
 
 
 # deque objects
+d1 = deque([1, 2, 3, 4])
+print('d1 =', d1)
+d1.append(1)
+d1.appendleft(-1)
+print('d1 =', d1)
+print('d1.count(1) =', d1.count(1))
+print('d1.count(5) =', d1.count(5))
+d1.extend([5, 6, 7, 8])
+d1.extendleft([-2, -3, -4])
+print('d1 =', d1)
+d1.insert(4, 0)
+d1.pop()
+d1.popleft()
+print('d1 =', d1)
+print('d1.index(0) =', d1.index(0))
+d1.remove(1)
+print('d1 =', d1)
+try:
+    d1.remove(100)
+except ValueError as e:
+    print('d1.remove(100): ', e)
+d1.rotate(2)
+print('d1 =', d1)
+d1.rotate(-2)
+print('d1 =', d1)
+d2 = deque(maxlen=4)
+print('d2 =', d2)
+d2.extend(['a', 'b', 'c', 'd'])
+print('d2 =', d2)
+d2.reverse()
+print('d2 =', d2)
+d2.append('e')
+print('d2 =', d2)
+d2.appendleft('x')
+print('d2 =', d2)
+d3 = d2.copy()
+print('d3 =', d3)
+
+
+# defaultdict
+print(dir(defaultdict))
+riddle = """1 was a racehorse
+2 was 12
+111 race
+2112
+"""
+s = defaultdict(int)
+for r in riddle:
+    s[r] +=1
+print('Letter in riddle:', sorted(s.items()))
+
+w = defaultdict(list)
+riddle_one_line = re.subn('\n', ' ', riddle)
+print(riddle_one_line)
+for word in riddle_one_line[0].split(' '):
+    w[len(word)].append(word)
+print('Words in riddle:', sorted(w.items()))
+
+u = defaultdict(set)
+for word in riddle_one_line[0].split(' '):
+    u[len(word)].add(word)
+print('Unique words in riddle:', sorted(u.items()))
+
+uc = defaultdict(lambda: defaultdict(int))
+for word in riddle_one_line[0].split(' '):
+    uc[len(word)][word] += 1
+print('Unique words in riddle with counts:', sorted(uc.items()))
+
+
+# OrderedDict()
+print(dir(OrderedDict))
+o = OrderedDict()
+for line in riddle.split('\n'):
+    for word in line.split(' '):
+        if word in o:
+            o[word] += 1
+        else:
+            o[word] = 1
+    print(o)
+o.move_to_end('was')
+print(o)
+o.popitem()
+print(o)
+del o['1']
+print(o)
+o['1'] = 100
+print(o)
+
+# Not doing UserDict, UserList and UserString right now.
+
