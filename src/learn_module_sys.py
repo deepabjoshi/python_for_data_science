1212850251583cabd8852b5e936e962ca24b8746
Deepa Joshi <deepabjoshi@gmail.com>
2022-11-28 19:51:53 +0530

    Exploring sys

---
+++ src/learn_module_sys.py
@@ -0,0 +1,19 @@
+import sys
+print(dir(sys))
+
+print(sys.maxsize)
+print(sys.maxunicode)
+
+print(sys.int_info)
+
+print(sys.float_info)
+print(sys.float_repr_style)
+
+print(sys.byteorder)
+
+print(sys.exc_info())
+print(sys.base_prefix)
+print(sys.base_exec_prefix)
+print(sys.version_info)
+
+print(sys.hash_info)
